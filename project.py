7848deeee8d262da85ef118dbc0b2427505980bb
justsheila678 <smercado2468@gmail.com>
2025-05-08 11:22:27 -0400

    added the REAL project file

---
+++ project.py
@@ -0,0 +1,12 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title="Financial Budget Tracker"
+    title = ft.Text(value="Your Budget Tracker")
+
+    moneySpent = ft.TextField(label="Input Money Spent")
+    expenseCats = ft.Dropdown(label="Expense Category", options=
+                              [ft.dropdown.Option("Food"), ft.dropdown.Option("Health/Medicine"), ft.dropdown.Option("Trips"), ft.dropdown("Wishlist"), ft.dropdown.Option("House Bills")])
+ 
+
+ft.app(target=main)
