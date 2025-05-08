fe2c170f93e59e3a95d4ada7655ce9c76aff7806
justsheila678 <smercado2468@gmail.com>
2025-05-08 11:27:15 -0400

    deleted unnecessary file and added 3 functions we need to do

--- project.py
+++ project.py
@@ -7,6 +7,11 @@ def main(page: ft.Page):
     moneySpent = ft.TextField(label="Input Money Spent")
     expenseCats = ft.Dropdown(label="Expense Category", options=
                               [ft.dropdown.Option("Food"), ft.dropdown.Option("Health/Medicine"), ft.dropdown.Option("Trips"), ft.dropdown("Wishlist"), ft.dropdown.Option("House Bills")])
- 
+    def addTransaction(e):
+        pass
+    def calcBalance(e):
+        pass
+    def deleteTransaction(e):
+        pass
 
 ft.app(target=main)
