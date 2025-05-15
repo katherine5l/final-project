55e7ba7da29696c2fe7568801d673dbe91870945
justsheila678 <smercado2468@gmail.com>
2025-05-15 10:26:29 -0400

    deleted add transaction function that does not work

--- project.py
+++ project.py
@@ -8,26 +8,5 @@ def main(page: ft.Page):
     expenseCats = ft.Dropdown(label="Expense Category", options=
                               [ft.dropdown.Option("Food"), ft.dropdown.Option("Health/Medicine"), ft.dropdown.Option("Trips"), ft.dropdown("Wishlist"), ft.dropdown.Option("House Bills")])
 
-    
-    def addTransaction(e):
-        try:
-            amount = float(money_input.vslue_
-            category_dropdown.value
-            if not category;
-                status.value = "Please select a category."
-        else:
-            transictions.append({"amount"; amount, "category"; category})
-            rseult.value = "Transactin added"
-            money_input.value = ""
-            category_dropdown.value = None
-        except ValueError:
-            result.value = "Enter a alid number"
-        page.update()
-                           
-    def calcBalance(e):
-        pass
-    def deleteTransaction(e):
-        pass
-
 ft.app(target=main)
 
