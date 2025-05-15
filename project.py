ae7856d0cd1e510ae99439c19c33e973cc8e311f
justsheila678 <smercado2468@gmail.com>
2025-05-15 11:00:44 -0400

    added a dropdown for the type of transaction, a total balance text box and changed the name of moneyspent to transAmount

--- project.py
+++ project.py
@@ -4,9 +4,11 @@ def main(page: ft.Page):
     page.title="Financial Budget Tracker"
     title = ft.Text(value="Your Budget Tracker")
 
-    moneySpent = ft.TextField(label="Input Money Spent")
+    totalBalance=ft.Text(value="Current Balance: ")
+    typeInput=ft.Dropdown(label="Type of Transaction", options= [ft.dropdown.Option("Income"), ft.dropdown.Option("Expense")])
+    transAmount = ft.TextField(label="Amount of Money")
     expenseCats = ft.Dropdown(label="Expense Category", options=
-                              [ft.dropdown.Option("Food"), ft.dropdown.Option("Health/Medicine"), ft.dropdown.Option("Trips"), ft.dropdown("Wishlist"), ft.dropdown.Option("House Bills")])
+                              [ft.dropdown.Option("Food"), ft.dropdown.Option("Health/Medicine"), ft.dropdown.Option("Trips"), ft.dropdown("Wishlist"), ft.dropdown.Option("House Bills"), ft.dropdown("Rent"), ft.dropdown("Other")])
 
 ft.app(target=main)
 
